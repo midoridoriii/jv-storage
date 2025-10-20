88446b8ce37ad6960e5bd5a85a777cd80dbad9b5
midoridoriii <valissa069@gmail.com>
2025-10-20 16:54:21 +0300

    implemented methods in storageImpl class, according task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,75 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] keys = new Object[10];
+    private Object[] values = new Object[10];
+    private int size = 0;
+    private final int CAPACITY = 10;
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setKeys(Object[] keys) {
+        this.keys = keys;
+    }
+
+    public Object[] getKeys() {
+        return keys;
+    }
+
+    public void setValues(Object[] values) {
+        this.values = values;
+    }
+
+    public Object[] getValues() {
+        return values;
+    }
+
+    public int getCAPACITY() {
+        return CAPACITY;
+    }
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key == null && keys[i] == null) {
+                return i;
+            } else if (key != null && key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = indexOfKey(key);
+        if (index != -1) {
+            values[index] = value;
+        } else if (size == CAPACITY) {
+            throw new RuntimeException("Storage is full");
+        } else {
+            keys[size] = key;
+            values[size] = value;
+            size = size + 1;
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = indexOfKey(key);
+        if (index == -1) {
+            return null;
+        }
+        return (V) values[index];
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
